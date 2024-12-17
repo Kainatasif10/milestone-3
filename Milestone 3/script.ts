1a709ad5472161467cc828180c340b7d50e3ad77
Kainat Asif <160526881+Kainatasif10@users.noreply.github.com>
2024-12-17 22:11:47 +0500

    Add files via upload

---
+++ Milestone 3/script.ts
@@ -0,0 +1,47 @@
+// get references to the form and display area
+const form = document.getElementById('resume-form') as HTMLFormElement;
+const resumeDisplayElement = document.getElementById('resume-display') as HTMLDivElement;
+
+// Handle form submission
+form.addEventListener('submit' , (event : Event ) => {
+    event.preventDefault();
+
+    //collect input values
+
+
+    const name = (document.getElementById('name') as HTMLInputElement).value;
+    const email = (document.getElementById('email') as HTMLInputElement).value;
+    const phone = (document.getElementById('phone') as HTMLInputElement).value;
+    const education = (document.getElementById('education') as HTMLTextAreaElement).value;
+    const experience = (document.getElementById('experience') as HTMLTextAreaElement).value;
+    const skills = (document.getElementById('skills') as HTMLTextAreaElement).value;
+    const profilePicture = (document.getElementById('profilePicture') as HTMLInputElement).files?.[0];
+
+    // Generate the resume content dynamically
+    const resumeHTML =`
+    <h2><b>Resume<b></h2>
+    <h3>Personal Information</h3>
+    <p><b>Name:</b> ${name} </p>
+    <p><b>Email:</b> ${email} </p>
+    <p><b>Phone:</b> ${phone} </p>
+
+    <h3>Education</h3>
+    <p>${education}</p>
+
+    <h3>Experience</h3>
+    <p>${experience}</p>
+
+    <h3>Skills</h3>
+    <p>${skills}</p>
+
+    `;
+
+    // Display the generated resume
+    if(resumeDisplayElement){
+        resumeDisplayElement.innerHTML = resumeHTML;
+    } else {
+        console.error('The resume display element is missing.');
+    }
+});
+
+
